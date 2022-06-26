cb8274cdb961539bfe9ad74d2bc69384e2136821
CFCFM <1812357607@qq.com>
2022-06-26 17:22:19 +0800

    初始界面配置完毕

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 3000,
+      host: 'localhost'
+    },
+    resolve: {
+      alias: {
+        '#': path.join(__dirname, 'src/components')
+      }
+    }
+  }
 })
